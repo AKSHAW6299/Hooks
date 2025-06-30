120848f77e394e0f0519b59e198f6c80086db6f1
Anand6262 <pyexpert62@gmail.com>
2025-06-30 17:11:40 +0530

    bbug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './views/home/Home'
+import UseState from './views/usestate1/UseState'
+import UseEffect from './views/useeffect2/UseEffect'
+import UseReducer from './views/usereducer3/UseReducer'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/usestate" element={<UseState />} />
+        <Route path="/useeffect" element={<UseEffect />} />
+        <Route path="/usereducer" element={<UseReducer />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
