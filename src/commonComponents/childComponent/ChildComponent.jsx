69bf87f8aa7a72102558faf81e26706adfd49d7a
Anand6262 <pyexpert62@gmail.com>
2025-07-03 04:13:18 +0530

    chanegs

--- src/commonComponents/childComponent/ChildComponent.jsx
+++ src/commonComponents/childComponent/ChildComponent.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 
 // We are wrapping our component to prevent unnecessary re-renders!
-
+// React.memo -> wrap -> component -> ChildComponent component tabhi re-render hoga jab props change honge
+// nahi to re-render nahi hoga!
 const ChildComponent = React.memo(function ChildComponent(props) {
     // console.log(props);
     console.log('child component got re-rendered again.');
